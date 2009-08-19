7f1f3e99123f27f31dfef555139b58d8e8a167e4
PJ Davis <pj.davis@gmail.com>
2009-08-19 15:50:32 -0500

    Releasing 0.1.4

--- lib/roart.rb
+++ lib/roart.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 module Roart
 
   # :stopdoc:
-  VERSION = '0.1.3'
+  VERSION = '0.1.4'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
