e7be6eb4d8431b07d5989422461cc00f42cd4d0d
Henning Kiel <henning.kiel@gmail.com>
2010-05-23 13:46:34 +0200

    Only extend Hash and String when ActiveSupport is not loaded. This allows the use of this gem inside Rails apps

--- lib/roart/core/string.rb
+++ lib/roart/core/string.rb
@@ -1,26 +1,28 @@
-# used from ActiveSupport
-# Copyright (c) 2005-2009 David Heinemeier Hansson
+unless defined?(ActiveSupport)
+  # used from ActiveSupport
+  # Copyright (c) 2005-2009 David Heinemeier Hansson
 
-class String
+  class String
   
-  def underscore
-    self.gsub(/::/, '/').
-      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-      gsub(/([a-z\d])([A-Z])/,'\1_\2').
-      tr("-", "_").
-      downcase
+    def underscore
+      self.gsub(/::/, '/').
+        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr("-", "_").
+        downcase
+    end
+  
+    def camelize
+      self.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+    end
+  
+    def humanize
+      self.gsub(/_id$/, "").gsub(/_/, " ").capitalize
+    end
+  
+    def blank?
+      self == ""
+    end
+  
   end
-  
-  def camelize
-    self.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
-  end
-  
-  def humanize
-    self.gsub(/_id$/, "").gsub(/_/, " ").capitalize
-  end
-  
-  def blank?
-    self == ""
-  end
-  
 end
