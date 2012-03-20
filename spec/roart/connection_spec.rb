d8e12c20384671cfa9ad3f9c38665c6b77e5adaa
Andriy Yanko <andriy.yanko@gmail.com>
2012-03-20 19:06:28 +0200

    Lazy load connection adapter

--- spec/roart/connection_spec.rb
+++ spec/roart/connection_spec.rb
@@ -3,13 +3,40 @@ require File.join(File.dirname(__FILE__), %w[ .. spec_helper])
 
 describe "Connection" do
 
-  describe 'get and post' do
+  before do
+    @options = {:server => 'server', :user => 'user', :pass => 'pass', :adapter => 'whatev'}
+  end  
+  
+  it "should lazy load connection adapter" do
+    @options[:adapter] = 'mechanize'
+    Roart::ConnectionAdapter.should_not_receive(:new)
+    connection = Roart::Connection.new(@options)
+  end
 
+  it 'should raise an exception if it doesnt have all the options' do
+    lambda{Roart::Connection.new(:user => 'bad')}.should raise_error
+  end
+  
+  it 'should give us the rest path' do
+    connection = Roart::Connection.new(@options)
+    connection.rest_path.should == 'server/REST/1.0/'
+  end
+  
+  it 'should give us back the whole thing' do
+    @options[:adapter] = 'mechanize'
+    mock_mech = mock('mech')
+    Roart::ConnectionAdapters::MechanizeAdapter.should_receive(:new).with(@options).and_return(mock_mech)
+    mock_mech.should_receive(:login).with(@options)
+    mock_mech.should_receive(:get).with('uri').and_return('body')
+    connection = Roart::Connection.new(@options)
+    connection.get('uri').should == 'body'
+  end
+
+  describe 'get and post' do
     before do
       @agent = mock("agent", :null_object => true)
-      @options = {:server => 'server', :user => 'user', :pass => 'pass', :adapter => 'whatev'}
       Roart::ConnectionAdapter.should_receive(:new).and_return(@agent)
-    end  
+    end
   
     it 'should respond to get' do
       @agent.should_receive(:get).with('some_uri').and_return('body')
@@ -22,29 +49,6 @@ describe "Connection" do
       connection = Roart::Connection.new(@options)
       connection.post("some_uri", "a payload").should == 'body'
     end
-
-  end
-  
-  it 'should raise an exception if it doesnt have all the options' do
-    lambda{Roart::Connection.new(:user => 'bad')}.should raise_error
-  end
-  
-  it 'should give us the rest path' do
-    @agent = mock("agent", :null_object => true)
-    @options = {:server => 'server', :user => 'user', :pass => 'pass', :adapter => 'whatev'}
-    Roart::ConnectionAdapter.should_receive(:new).and_return(@agent)
-    connection = Roart::Connection.new(@options)
-    connection.rest_path.should == 'server/REST/1.0/'
-  end
-  
-  it 'should give us back the whole thing' do
-    mock_mech = mock('mech')
-    @options = {:server => 'server', :user => 'user', :pass => 'pass', :adapter => 'mechanize'}
-    Roart::ConnectionAdapters::MechanizeAdapter.should_receive(:new).with(@options).and_return(mock_mech)
-		mock_mech.should_receive(:login).with(@options)
-    mock_mech.should_receive(:get).with('uri').and_return('body')
-    connection = Roart::Connection.new(@options)
-    connection.get('uri').should == 'body'
   end
   
 end 
