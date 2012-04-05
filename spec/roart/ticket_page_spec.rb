6f756018da81c69b0099eb922ef04d476c67ae9d
Andriy Yanko <andriy.yanko@gmail.com>
2012-04-05 13:46:33 +0300

    Properly parse ticket response that contains CF multiline values

--- spec/roart/ticket_page_spec.rb
+++ spec/roart/ticket_page_spec.rb
@@ -1,9 +1,9 @@
-require File.join(File.dirname(__FILE__), %w[ .. spec_helper])
+require 'spec_helper'
 
 describe 'ticket page' do
-  
+
   describe 'ticket hash' do
-    
+
     it 'should convert an array to a hash' do
       array = ["id : 10", "subject : asdf"]
       array.extend(Roart::TicketPage)
@@ -12,82 +12,117 @@ describe 'ticket page' do
       hash[:id].should == 10
       hash[:subject].should == 'asdf'
     end
-  
+
   end
 
-	describe 'reading a ticket' do
-		
-		before do
-			@page = File.open(File.join(File.dirname(__FILE__), %w[ .. test_data ticket.txt])).readlines.join
-			@page = @page.split("\n")
+  describe 'reading a ticket' do
+
+    before do
+      @page = File.open(File.join(File.dirname(__FILE__), %w[ .. test_data ticket.txt])).readlines.join
+      @page = @page.split("\n")
       @page.extend(Roart::TicketPage)
-		end
-		
-		it 'should include custom fields' do
-			@page.to_hash[:cf_BTN].should == '1035328269'
-		end
-		
-		it 'should be a hash' do
-			@page.to_hash.class.should == HashWithIndifferentAccess
-		end
-		
-	end
-  
+    end
+
+    subject { @page.to_hash }
+
+    it 'should be a hash' do
+      subject.class.should == HashWithIndifferentAccess
+    end
+
+    it 'should include id' do
+      subject[:id].should == 358171
+    end
+
+    it 'should include regular fields' do
+      subject[:subject].should == 'MyQueue - some_guy - 1035328269 - DSL Modem Reset ESCALATED'
+    end
+
+    it 'should include custom fields with old format' do
+      subject[:cf_BTN].should == '1035328269'
+    end
+
+    it 'should include custom fields with new format' do
+      subject[:cf_NEW_BTN].should == '2035328269'
+    end
+
+    it 'should properly build old formatted custom fields with multiple line value' do
+      subject['cf_Contact_Source'].should == (<<-DATA).strip
+<contact-info>
+      <name>Jane Smith</name>
+      <company>AT&amp;T</company>
+      <phone>(212) 555-4567</phone>
+    </contact-info>
+DATA
+    end
+
+    it 'should properly build new formatted custom fields with multiple line value' do
+      subject['cf_Feed_Source'].should == (<<-DATA).strip
+<item>
+    <title>Example entry</title>
+    <description>Here is some text containing an interesting description.</description>
+    <link>http://www.wikipedia.org/</link>
+    <guid>unique string per item</guid>
+    <pubDate>Mon, 06 Sep 2009 16:45:00 +0000 </pubDate>
+  </item>
+DATA
+    end
+  end
+
   describe 'search array' do
-    
+
     before do
       @array = ["123 : Subject", "234 : Subject"]
       @array.extend(Roart::TicketPage)
       @array = @array.to_search_array
     end
-    
+
     it 'should make an array of search results' do
       @array.size.should == 2
     end
-    
+
     it 'should put search elements into the search array' do
       @array.first[:id].should == 123
       @array.last[:id].should == 234
     end
-    
+
   end
 
-	describe "search list array" do
-	  before do
-			@array = [['id:234', 'subject:SomeTicket', ], ['id:432','subject:Another']]
+  describe "search list array" do
+    before do
+      @array = [['id:234', 'subject:SomeTicket', ], ['id:432','subject:Another']]
       @array.extend(Roart::TicketPage)
       @array = @array.to_search_list_array
-		end
-		
-		it "should return an array of hashes" do
-		  @array.first.class.should == HashWithIndifferentAccess
-		end
-		
-		it "should put the search elements into the array" do
-			@array.first[:id].should == 234
-		end
-	end
-  
+    end
+
+    it "should return an array of hashes" do
+      @array.first.class.should == HashWithIndifferentAccess
+    end
+
+    it "should put the search elements into the array" do
+      @array.first[:id].should == 234
+    end
+  end
+
   describe 'ticket history hash' do
-    
+
     before do
       @page = File.open(File.join(File.dirname(__FILE__), %w[ .. test_data single_history.txt])).readlines.join
       @page = @page.split("\n")
       @page.extend(Roart::TicketPage)
     end
-    
+
     it 'should give back the hash of history' do
       @page.to_history_hash.class.should == HashWithIndifferentAccess
     end
-    
+
     it 'should have some content' do
-      @page.to_history_hash[:content].should_not be_nil
+      @page.to_history_hash[:content].should == "Now you can get big real fast at an affordable price\nhttp://www.lameppe.com/"
     end
-    
+
     it 'should have the create type' do
       @page.to_history_hash[:type].should == 'Create'
     end
-    
+
   end
-  
+
 end
